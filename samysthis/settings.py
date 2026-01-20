4f06f4fe80f3120150edbc049ccd36ce8b171799
yhubham <shubhamyadav.whoishe@gmail.com>
2026-01-20 12:28:42 +0530

    Fix STATIC_ROOT for Render

--- samysthis/settings.py
+++ samysthis/settings.py
@@ -114,4 +114,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
