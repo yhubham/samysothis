c583ab244872896409997902a018774a515b5613
yhubham <shubhamyadav.whoishe@gmail.com>
2026-01-20 13:21:10 +0530

    Initial commit - added Django project and coming soon page

--- samysthis/settings.py
+++ samysthis/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,8 @@ ROOT_URLCONF = 'samysthis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,5 +119,6 @@ USE_TZ = True
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
